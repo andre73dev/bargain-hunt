312f12e6dc98b8e4dcf8989355e21e6172b581d9
andre73dev <andre73.dev@gmail.com>
2019-01-06 23:11:09 +0900

    rakuten config

--- config/environment.rb
+++ config/environment.rb
@@ -11,10 +11,3 @@ Amazon::Ecs.options = {
     AWS_secret_key:   Rails.application.secrets.aws_secret_key
 }
 
-#RakutenWebServiceのAPIキーをsercrets.ymlから取得する関数
-RakutenWebService.configuration do |c|
-    # (Required) Appliction ID for your application.
-    c.application_id = Rails.application.secrets.rakuten_application_id
-    # (Optional) Affiliate ID for your Rakuten account.
-    c.affiliate_id = Rails.application.secrets.rakuten_affiliate_id
-end
---
+++ config/initializers/rakuten.rb
@@ -0,0 +1,7 @@
+#RakutenWebServiceのAPIキーをsercrets.ymlから取得する関数
+RakutenWebService.configuration do |c|
+    # (Required) Appliction ID for your application.
+    c.application_id = Rails.application.secrets.rakuten_application_id
+    # (Optional) Affiliate ID for your Rakuten account.
+    c.affiliate_id = Rails.application.secrets.rakuten_affiliate_id
+end
